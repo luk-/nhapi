98cecdbf99e4caf77a5fc61fc88bde2f14a04035
Luke <luke.arduini@me.com>
2012-11-20 18:14:38 -0500

    Auth method to retrieve key & secret

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var HapiClient = require('./lib').HapiClient
+var hapi = require('./lib')
 
 exports.create = function (auth) {
   return new HapiClient(auth)
+}
+
+exports.getCredentials = function (auth, cb) {
+  return hapi.getCredentials(auth, cb)
 }
